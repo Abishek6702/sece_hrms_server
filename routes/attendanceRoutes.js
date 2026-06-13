572da4af1bc1e289afd7b90de45446c40983fd57
Abishek6702 <abishekkrishnat@gmail.com>
2026-06-13 10:23:04 +0530

    protect

--- routes/attendanceRoutes.js
+++ routes/attendanceRoutes.js
@@ -11,7 +11,7 @@ const protect = require("../middleware/protect");
 const router = express.Router();
 
 router.get("/muster", protect, getAttendanceMuster);
-router.get("/faculty-attendance", getFacultyAttendanceHistory);
+router.get("/faculty-attendance", protect, getFacultyAttendanceHistory);
 router.get("/week-summary", protect, getAttendanceWeekSummary);
 router.get("/my-summary",protect, getMyAttendanceSummary);
 
