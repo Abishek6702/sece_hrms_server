bac938677fcb324afa9320ef218bb5b458d780f2
ZSathiya99 <sathiyavijaya1999@gmail.com>
2026-07-06 17:00:24 +0530

    update slotadd

--- models/permission.js
+++ models/permission.js
@@ -19,6 +19,12 @@ const permissionSchema = new mongoose.Schema(
       default: "Personal",
     },
 
+    slot: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
     fromTime: {
       type: String,
       required: true,
